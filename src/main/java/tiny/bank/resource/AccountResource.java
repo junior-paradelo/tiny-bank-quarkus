7c59bc283c1313263ad9ce56732552f492f6be00
Junior Paradelo <juniorparadelo@gmail.com>
2024-04-11 19:56:24 +0200

    feat: new operation to create an account

--- src/main/java/tiny/bank/resource/AccountResource.java
+++ src/main/java/tiny/bank/resource/AccountResource.java
@@ -3,7 +3,9 @@ package tiny.bank.resource;
 import java.util.List;
 
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import tiny.bank.model.Account;
 import tiny.bank.repository.AccountRepository;
@@ -17,5 +19,12 @@ public class AccountResource {
 	@GET
 	public List<Account> index() {
 		return accountRepository.listAll();
+	}
+	
+	@POST
+	@Transactional
+	public Account insert(Account insertedAccount) {
+		accountRepository.persist(insertedAccount);
+		return insertedAccount;
 	}
 }
